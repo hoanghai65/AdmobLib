aae93e472c2a0919bd8a17eddeb5fba37a2ddc61
HoangHai <hoangduchai_t64@hus.edu.vn>
2024-02-16 00:47:08 +0700

    first release

--- admoblibrary/build.gradle.kts
+++ admoblibrary/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("com.android.library")
 //    id("com.android.application")
@@ -7,17 +6,14 @@ plugins {
 }
 
 android {
-    namespace = "com.haihd1.admoblib"
+    namespace = "com.haihd1.abmoblibrary"
     compileSdk = 33
 
     defaultConfig {
-//        applicationId = "com.haihd1.admoblib"
         minSdk = 24
-        targetSdk = 33
-//        versionCode = 1
-//        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -33,7 +29,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
 }
 java {
     toolchain {
@@ -44,14 +39,16 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17            // << --- ADD This
     targetCompatibility = JavaVersion.VERSION_17
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     implementation("com.google.android.gms:play-services-ads:22.6.0")
     implementation("com.google.android.ump:user-messaging-platform:2.2.0")
     implementation("com.facebook.shimmer:shimmer:0.5.0")
